ffee77c686675627a2a0c3a46ae45e00abd4a512
MUDGAL Prateeksha <PML1@dsone.3ds.com>
2016-08-23 22:05:17 -0400

    final update

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const port = process.env.PORT || '8080';
 const host = process.env.HOST || '0.0.0.0';
-const express = require('express');
-const app = express();
+//var port = '8080';
+//var host = '0.0.0.0';
+var express = require('express');
+var app = express();
+
+var rules = require('./app/');
+app.use(rules);
 
 app.listen(port,host);
 console.log("Listening on : "+host+":"+port);
