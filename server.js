f382eb577959591dd8825f36547f0791bedef419
MUDGAL Prateeksha <PML1@dsone.3ds.com>
2016-08-23 21:07:08 -0400

    make server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-require('express');
+const port = process.env.PORT || '8080';
+const host = process.env.HOST || '0.0.0.0';
+const express = require('express');
+const app = express();
+
+app.listen(port,host);
+console.log("Listening on : "+host+":"+port);
